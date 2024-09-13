3bfa06e11713d6a397930e0d073ff678e60d6d20
felipoxy1 <luizfelipepassos2015@gmail.com>
2024-09-12 23:33:24 -0300

    Feat: Home-banner

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,10 +1,13 @@
 import Navbar from "../../shared/navbar/navbar";
+import HomerBanner from "./components/home-banner/home-banner";
 
 function index() {
     return ( 
 
         <>
         <Navbar />
+
+        <HomerBanner />
         </>
     );
 }
