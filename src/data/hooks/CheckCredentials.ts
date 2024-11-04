8e9b049de7e89edc944e6fefa00f3c920760fc0c
AndreSilvazm <andre.mani2015@gmail.com>
2024-11-03 23:09:44 -0300

    :sparkles: Feat: Criação do Hook que faz a verificação das credenciais de Login

---
+++ src/data/hooks/CheckCredentials.ts
@@ -0,0 +1,34 @@
+interface ErrorsProps {
+  Error: string
+}
+
+export function CheckIfPasswordIsCorrect(Credential:string){
+
+  const Errors:ErrorsProps = {
+    Error: ''
+  }
+
+  /*Parte condicional: Você pode adicionar mais erros abaixo!*/
+  if(Credential == ''){
+
+    return Errors.Error = "O campo Senha é obrigatório."
+  }
+
+  return Errors.Error
+}
+
+
+export function CheckIfEmailIsCorrect(Credential:string){
+
+  const Errors:ErrorsProps = {
+    Error: ''
+  }
+
+  /*Parte condicional: Você pode adicionar mais erros abaixo!*/
+  if(Credential == ''){
+
+    return Errors.Error = "O campo E-mail é obrigatório."
+  }
+
+  return Errors.Error
+}
