89d1fc695b26138b51ab77c86319362080e635be
AndreSilvazm <andre.mani2015@gmail.com>
2024-11-09 15:58:36 -0300

    segurando arquivos

---
+++ src/shared/ShowGraadQRCode/index.tsx
@@ -0,0 +1,42 @@
+function ShowGraadQRCode() {
+  return (
+    <section>
+      <article>
+        <h1>Ajude com qualquer valor</h1>
+        <h2>Escaneie o QR Code para efetuar doação</h2>
+      </article>
+
+      <section>
+
+        <div>
+          <img src="https://arcanimal.com.br/assets/img/donation-code.svg" alt="PIX QRCODE" />
+        </div>
+
+        <article>
+          <h1>
+            <span>Chave:</span> doe@gradbrasil.org.brdoe@gradbrasil.org.br
+          </h1>
+          <h2>
+            Todas as doações serão feitas diretamente na conta do GRAD Brasil
+          </h2>
+        </article>
+
+        <div>
+          <button>Copiar código PIX</button>
+        </div>
+      </section>
+
+      <article>
+        <h1>Pix com QR Code, como fazer?</h1>
+        <hr />
+        <p>
+          Aponte a câmera do seu celular para o QR Code e faça um Pix no app do
+          seu banco. Se for no banco Inter, você também pode baixar essa imagem
+          e buscar o QR Code na sua galeria.
+        </p>
+      </article>
+    </section>
+  );
+}
+
+export default ShowGraadQRCode;
